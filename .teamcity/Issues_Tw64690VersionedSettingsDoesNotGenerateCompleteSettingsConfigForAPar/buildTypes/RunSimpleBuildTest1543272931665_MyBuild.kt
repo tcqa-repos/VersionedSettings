872e2fb78271a8fcf37f9ef38705c0dbd6f99d0b
Repository with test projects <teamcity.qa.server@gmail.com>
2020-03-03 14:01:37 +0300

    Update RunSimpleBuildTest1543272931665_MyBuild.kt

--- .teamcity/Issues_Tw64690VersionedSettingsDoesNotGenerateCompleteSettingsConfigForAPar/buildTypes/RunSimpleBuildTest1543272931665_MyBuild.kt
+++ .teamcity/Issues_Tw64690VersionedSettingsDoesNotGenerateCompleteSettingsConfigForAPar/buildTypes/RunSimpleBuildTest1543272931665_MyBuild.kt
@@ -4,6 +4,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.nuGetPublish
 
 object RunSimpleBuildTest1543272931665_MyBuild : BuildType({
+    uuid = "77fb03b4-0c81-4a2f-8f60-8134c0ff37cd"
     name = "Build2"
 
     steps {
